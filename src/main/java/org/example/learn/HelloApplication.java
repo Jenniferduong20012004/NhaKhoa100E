51cb8e4ce5a1ca54b0e6fcdf1db0f06b45a73fe7
Itsjennyyahh <56812916+Jenniferduong20012004@users.noreply.github.com>
2024-04-23 10:59:44 +0700

    add function

--- src/main/java/org/example/learn/HelloApplication.java
+++ src/main/java/org/example/learn/HelloApplication.java
@@ -10,10 +10,15 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setScene(scene);
-        stage.show();
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/org/example/learn/HelloController.java
+++ src/main/java/org/example/learn/HelloController.java
@@ -1,14 +1,25 @@
 package org.example.learn;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label myLabel;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextArea textField1;
+    @FXML
+    private Button saveButton;
+    String name;
+    @FXML
+    public void submit (ActionEvent event){
+        name = textField1.getText();
+        System.out.println (name);
     }
+
 }

a84d4b6403a37da52a219ac7adad81545f829e5f
Itsjennyyahh <56812916+Jenniferduong20012004@users.noreply.github.com>
2024-04-29 16:19:45 +0700

    patientlog

---
+++ src/main/java/org/example/learn/PatientPageControl.java
@@ -0,0 +1,37 @@
+package org.example.learn;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.example.learn.HelloApplication;
+
+public class PatientPageControl{
+    private Stage stage;
+    private Scene scene;
+    private FXMLLoader fxmlLoader;
+    public void switchToEquipment(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("equipment.fxml"));
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(fxmlLoader.load());
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void switchToTreatment(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("tracking.fxml"));
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(fxmlLoader.load());
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}

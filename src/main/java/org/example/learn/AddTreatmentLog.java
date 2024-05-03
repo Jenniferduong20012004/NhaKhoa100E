1067d41729c4eed5bae0529cb2b842780113b0e8
Itsjennyyahh <56812916+Jenniferduong20012004@users.noreply.github.com>
2024-05-03 13:05:01 +0700

    add treatment

--- src/main/java/org/example/learn/AddTreatmentLog.java
+++ src/main/java/org/example/learn/AddTreatmentLog.java
@@ -5,11 +5,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class AddTreatmentLog {
     private Stage stage;
@@ -21,16 +21,52 @@ public class AddTreatmentLog {
     @FXML
     private TextField textField2;
     @FXML
-    private TextField textField3;
+    private DatePicker datePicker;
     @FXML
     private TextArea textArea;
     @FXML
     private Button saveButton;
+    @FXML
+    private Label amountlabel;
+
+    @FXML
+    private Label criterialabel;
+    @FXML
+    private Label labolabel;
+    @FXML
+    private TextField textField3;
+
+    @FXML
+    private TextField textField4;
+
+    @FXML
+    private TextField textField5;
+    @FXML
+    public void clickYes(ActionEvent event){
+        textField3.setVisible(true);
+        textField4.setVisible(true);
+        textField5.setVisible(true);
+        amountlabel.setVisible(true);
+        criterialabel.setVisible(true);
+        labolabel.setVisible(true);
+    }
 
     @FXML
     public void collectInformation (ActionEvent event){
         String name = textField1.getText();
         String contactnumber = textField2.getText();
+        LocalDate localDate = datePicker.getValue();
+        String pattern = "MMMM dd, yyyy";
+        String datePattern = localDate.format(DateTimeFormatter.ofPattern(pattern));
+        String Description = textArea.getText();
+        String sql = "Insert into Treatment Values (?,?,?,?)";
+        if (name.isEmpty()||contactnumber.isEmpty()||datePattern.isEmpty()){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setContentText("Please fill missing data");
+            alert.showAndWait();
+        }
+
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Tracking.fxml"));
             stage = (Stage)((Node)event.getSource()).getScene().getWindow();

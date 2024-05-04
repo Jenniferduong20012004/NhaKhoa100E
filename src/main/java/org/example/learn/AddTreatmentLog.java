b081a74760b7f028f04caad86e96c1020b8afe8d
Itsjennyyahh <56812916+Jenniferduong20012004@users.noreply.github.com>
2024-05-04 09:25:34 +0700

    add save patient and check patient id

--- src/main/java/org/example/learn/AddTreatmentLog.java
+++ src/main/java/org/example/learn/AddTreatmentLog.java
@@ -8,74 +8,153 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
 
+import javax.swing.*;
+import java.sql.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class AddTreatmentLog {
+    @FXML
+    private Label DescriptionLabel;
+    @FXML
+    private Button checkButton;
     private Stage stage;
     private Scene scene;
     private FXMLLoader fxmlLoader;
 
     @FXML
-    private TextField textField1;
-    @FXML
-    private TextField textField2;
-    @FXML
-    private DatePicker datePicker;
-    @FXML
-    private TextArea textArea;
-    @FXML
-    private Button saveButton;
-    @FXML
     private Label amountlabel;
 
     @FXML
     private Label criterialabel;
+
+    @FXML
+    private DatePicker datePicker;
+
+    @FXML
+    private DatePicker datePickerdob;
+
     @FXML
     private Label labolabel;
+    @FXML
+    private RadioButton radioButtonNo;
+
+    @FXML
+    private RadioButton radioButton;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private Button saveButton1;
+
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    private TextField textField1;
+
     @FXML
     private TextField textField3;
 
     @FXML
     private TextField textField4;
-
     @FXML
     private TextField textField5;
     @FXML
+    private Label Date;
+    private boolean clicked= false;
+    private Connection connection;
+    private PreparedStatement pst = null;
+    private ResultSet rs = null;
+    @FXML
+    void CheckData(ActionEvent event) {
+            String name = textField1.getText();
+            LocalDate localDate = datePickerdob.getValue();
+            java.sql.Date dateSQL = java.sql.Date.valueOf(localDate);
+            String pattern = "yyyy-MMMM-dd";
+            String datePattern = localDate.format(DateTimeFormatter.ofPattern(pattern));
+            if (name.isEmpty() ||  datePattern.isEmpty()) {
+                JOptionPane.showMessageDialog(null, "Please fill in missing data");
+        }
+            String sql = "DECLARE @class nvarchar(50) = N'Như'"+ " Select patient_id from Patient where namePatient = @class and dateOfBirth = ?";
+            System.out.println(sql);
+            connection = JDBConnection.NhaKhoa100eConnect();
+        try {
+            pst = connection.prepareStatement(sql);
+            pst.setString(1, datePattern);
+            rs = pst.executeQuery();
+            if (rs.isBeforeFirst()){
+                System.out.println(localDate);
+                Date.setVisible(true);
+                DescriptionLabel.setVisible(true);
+                textArea.setVisible(true);
+                datePicker.setVisible(true);
+                radioButtonNo.setVisible(true);
+                radioButton.setVisible(true);
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Patient first come to clinic insert in New Patient first");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @FXML
     public void clickYes(ActionEvent event){
-        textField3.setVisible(true);
-        textField4.setVisible(true);
-        textField5.setVisible(true);
-        amountlabel.setVisible(true);
-        criterialabel.setVisible(true);
-        labolabel.setVisible(true);
+        if (clicked == false){
+            clicked = true;
+            textField3.setVisible(true);
+            textField4.setVisible(true);
+            textField5.setVisible(true);
+            amountlabel.setVisible(true);
+            criterialabel.setVisible(true);
+            labolabel.setVisible(true);
+        }
+        else{
+           clicked = false;
+            textField3.setVisible(false);
+            textField4.setVisible(false);
+            textField5.setVisible(false);
+            amountlabel.setVisible(false);
+            criterialabel.setVisible(false);
+            labolabel.setVisible(false);
+        }
     }
 
     @FXML
-    public void collectInformation (ActionEvent event){
+    public void collectInformation (ActionEvent event) throws SQLException {
         String name = textField1.getText();
-        String contactnumber = textField2.getText();
         LocalDate localDate = datePicker.getValue();
+        java.sql.Date date = java.sql.Date.valueOf(localDate);
         String pattern = "MMMM dd, yyyy";
         String datePattern = localDate.format(DateTimeFormatter.ofPattern(pattern));
+        System.out.println(name + datePattern);
         String Description = textArea.getText();
         String sql = "Insert into Treatment Values (?,?,?,?)";
-        if (name.isEmpty()||contactnumber.isEmpty()||datePattern.isEmpty()){
+        if (name.isEmpty() ||  datePattern.isEmpty()) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setHeaderText(null);
             alert.setContentText("Please fill missing data");
             alert.showAndWait();
-        }
+        } else {
+                try {
+                    connection = JDBConnection.NhaKhoa100eConnect();
+                    pst = connection.prepareStatement(sql);
+                    pst.setString(1, name);
+                    pst.setDate(3, date);
+                    pst.setString(4,Description);
+                    int i = pst.executeUpdate();
+                    if (i == 1) {
+                        JOptionPane.showMessageDialog(null, "Save data successfully");
+                    }
+                }
+                catch (SQLException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    pst.close();
+                }
 
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Tracking.fxml"));
-            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            scene = new Scene(fxmlLoader.load());
-            stage.setScene(scene);
-            stage.show();
-        }
-        catch(Exception e){
-            e.printStackTrace();
         }
     }
     public void switchToEquipment(ActionEvent event) {
--- src/main/java/org/example/learn/addNewPatient.java
+++ src/main/java/org/example/learn/addNewPatient.java
@@ -7,13 +7,17 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.net.URL;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 import javax.swing.JOptionPane;
 
@@ -24,13 +28,20 @@ public class addNewPatient implements Initializable {
     private Scene scene;
     private FXMLLoader fxmlLoader;
     private Connection connection;
+    @FXML
+    private DatePicker datePicker;
+
     private PreparedStatement pst = null;
     @FXML
     public void getInformation (ActionEvent event) throws SQLException {
-        String sql = "Insert into Patient Values (?,?,?)";
+        String sql = "Insert into Patient (namePatient, dateOfBirth, contactNumber, addressPatient) Values (?,?,?,?)";
         String name = textField.getText();
         String contactNumber = textField1.getText();
         String address= textField2.getText();
+        LocalDate localDate = datePicker.getValue();
+        Date date = Date.valueOf(localDate);
+        String pattern = "MMMM dd, yyyy";
+        String datePattern = localDate.format(DateTimeFormatter.ofPattern(pattern));
         if (name.isEmpty()||contactNumber.isEmpty()||address.isEmpty()){
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setHeaderText(null);
@@ -42,11 +53,11 @@ public class addNewPatient implements Initializable {
                 connection =JDBConnection.NhaKhoa100eConnect();
                 pst = connection.prepareStatement(sql);
                 pst.setString(1, name);
-                pst.setString(2, contactNumber);
-                pst.setString(3, address);
+                pst.setDate(2,date);
+                pst.setString(3, contactNumber);
+                pst.setString(4, address);
                 int i =pst.executeUpdate();
                 if (i==1){
-                    System.out.println("Sucess");
                     JOptionPane.showMessageDialog(null, "Save data successfully");
                 }
 

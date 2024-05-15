8acc528c12b80ef798080666d50f0b403565a52b
Itsjennyyahh <56812916+Jenniferduong20012004@users.noreply.github.com>
2024-05-15 19:52:44 +0700

    storeprocedure

--- src/main/java/ViewModel/LaboratoryUseVM.java
+++ src/main/java/ViewModel/LaboratoryUseVM.java
@@ -6,10 +6,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import org.example.learn.Equipment;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,7 +14,7 @@ public class LaboratoryUseVM {
     ObservableList<LaboratoryUse> laboUseList = FXCollections.observableArrayList();
     Connection connection = null;
     ResultSet rs = null;
-    PreparedStatement pst = null;
+    CallableStatement call;
 
     public ObservableList<LaboratoryUse> getLaboUseList() {
         return laboUseList;
@@ -31,8 +28,9 @@ public class LaboratoryUseVM {
     private void loadDataFromDatabase() {
         try {
             laboUseList.clear();
-            pst = connection.prepareStatement("Select Patient.namePatient, labUse.dateCome, labUse.Criteria, labUse.Quantity, labUse. laboName from Patient, labUse where Patient.patient_id = labUse.patient_id");
-            rs = pst.executeQuery();
+            call = connection.prepareCall("{call getLaboratory}");
+            call.execute();
+            rs = call.getResultSet();
             while (rs.next()){
                 laboUseList.add(new LaboratoryUse(rs.getString("laboName"),//tên cột trong sql
                         rs.getString("namePatient"),

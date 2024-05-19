1fc2ccf35d5544fa961522149a85cc7e8e47fbbe
Itsjennyyahh <56812916+Jenniferduong20012004@users.noreply.github.com>
2024-05-19 21:26:30 +0700

    add

--- src/main/java/org/example/learn/PatientImageController.java
+++ src/main/java/org/example/learn/PatientImageController.java
@@ -1,8 +1,10 @@
 package org.example.learn;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 
 public class PatientImageController {
+    @FXML
     public void onViewButtonClicked(ActionEvent event) {
     }
 
